1e24f1d5f78bd34064de56337bb8887f00a699c4
Lucas Yamaguti <lucasycosta@gmail.com>
2021-12-09 19:38:47 -0300

    primeiro commit

---
+++ src/main/java/projeto/maven/model/Filme.java
@@ -0,0 +1,45 @@
+package projeto.maven.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Filme {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String nome;
+	private String genero;
+	
+	public Filme() {
+	}
+	
+	public Filme(Integer id, String nome, String genero) {
+		this.id = id;
+		this.nome = nome;
+		this.genero = genero;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/main/java/projeto/maven/persistence/FilmeDao.java
@@ -0,0 +1,113 @@
+package projeto.maven.persistence;
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import projeto.hibernate.model.Filme;
+
+public class FilmeDao {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Filme f1 = new Filme();
+		char resp = 0;
+		
+		// CONEXÃO COM O BANCO DE DADOS
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("filme2");
+		EntityManager em = emf.createEntityManager();
+		
+		// INICIA UMA TRANSANÇÃO COM O BANCO DE DADOS
+		em.getTransaction().begin();
+
+		do {
+			System.out.print("\n O que você deseja fazer?");
+			System.out.println("\n |1 - incluir novo filme" + 
+							   "\n |2 - consultar file existente" + 
+							   "\n |3 - alterar os dados de um filme" + 
+							   "\n |4 - deletar o cadastro de um filme");
+			System.out.print("Opção desejada: ");
+			int opcao = input.nextInt();
+
+			switch (opcao) {
+			case 1: {
+				// ENTRADA DE DADOS
+				System.out.println("digite um nome: ");
+				f1.setNome(input.next());
+				System.out.println("digite um genero: ");
+				f1.setGenero(input.next());
+				em.persist(f1);
+
+				// CONFIRMA AS ALTERAÇÕES
+				em.getTransaction().commit();
+				System.out.println("Filme adicionado!!!");
+
+				break;
+			}
+
+			case 2: {
+				try {
+					System.out.print("Digite o ID do filme: ");
+					int id = input.nextInt();
+					// FUNÇÃO PARA BUSCAR OBJETO PELO ID
+					Filme f = em.find(Filme.class, id);
+					System.out.println("Nome do Filme: " + f.getNome());
+					System.out.println("Genero do Filme: " + f.getGenero());
+				} catch (Exception e) {
+					System.out.println("\n ID invalido!!!");
+				}
+				break;
+			}
+			case 3: {
+				try {
+					System.out.print("Digite o ID do filme: ");
+					int id = input.nextInt();
+					Filme f3 = em.find(Filme.class, id);
+					System.out.println("\n Nome atual do Filme: " + f3.getNome());
+					System.out.println("\n Genero atual do Filme: " + f3.getGenero());
+					System.out.println("\n Digite o novo nome do filme: ");
+					String nome = input.next();
+					System.out.println("\n Digite o novo genero do filme: ");
+					String genero = input.next();
+
+					f3.setNome(nome);
+					f3.setGenero(genero);
+
+					em.merge(f3);
+					em.getTransaction().commit();
+					System.out.println("Dados atualizados!!!");
+					
+				} catch (Exception e) {
+					System.out.println("\n ID invalido!!!");
+				}
+				break;
+			}
+
+			case 4:
+				try {
+					System.out.print("Digite o ID do filme: ");
+					int id = input.nextInt();
+					Filme f2 = em.find(Filme.class, id);
+					em.remove(f2);
+					em.getTransaction().commit();
+					System.out.println("Filme Excluido!!!!");
+					
+				} catch (Exception e) {
+					System.out.println("\n ID invalido!!!");
+				}
+				break;
+			}
+
+			System.out.println("\n Deseja continuar? s ou n");
+			resp = input.next().charAt(0);
+			
+		} while (resp != 'n');
+		
+		if(resp == 'n') {
+			System.out.println("Operação finalizada!!");
+		}
+		
+		input.close();
+	}
+}
